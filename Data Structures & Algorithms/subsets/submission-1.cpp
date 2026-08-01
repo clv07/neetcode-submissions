70b4c7620cb88d68c21e88ab6f3fadc87a415dc5
Livia Chandra <76584280+clv07@users.noreply.github.com>
2026-07-31 18:38:49 -0700

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        // initialize with empty vector
+        vector<vector<int>> res = {{}};
+
+        for (int i = 0; i < nums.size(); i++) {
+            int size = res.size();
+            for (int j = 0; j < size; j++) {
+                vector<int> subset = res[j];
+                subset.push_back(nums[i]);
+                res.push_back(subset);
+            }
+        }
+
+        return res;
+    }
+};
