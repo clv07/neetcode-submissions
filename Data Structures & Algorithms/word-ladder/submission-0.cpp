9bd701ce59efdc239c7f577487dc76ea1c4f1f8f
Livia Chandra <76584280+clv07@users.noreply.github.com>
2026-08-10 12:54:53 -0700

    Add: word-ladder - submission-0

---
+++ Data Structures & Algorithms/word-ladder/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> words(wordList.begin(), wordList.end());
+        if (words.find(endWord) == words.end() || beginWord == endWord) return 0;
+
+        int res = 0;
+        queue<string> q;
+        q.push(beginWord);
+
+        while(!q.empty()) {
+            res++;
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                string node = q.front();
+                q.pop();
+                if (node == endWord) return res;
+                // generate neighbours
+                for (int j = 0; j < node.length(); j++) {
+                    char original = node[j];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == original) continue;
+                        node[j] = c;
+                        if (words.find(node) != words.end()) {
+                            q.push(node);
+                            words.erase(node);
+                        }
+                    }
+                    node[j] = original;
+                }
+            }
+        }
+
+        return 0;
+    }
+};
