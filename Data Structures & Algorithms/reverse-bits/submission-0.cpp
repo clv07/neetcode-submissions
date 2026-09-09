596d925c82786e9f06748a8a592a560b9f53fa13
Livia Chandra <76584280+clv07@users.noreply.github.com>
2026-09-09 13:40:03 -0700

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans = 0, offset = 31;
+        while(n) {
+            if (n & 1) ans += (1 << offset);
+            offset--;
+            n >>= 1;
+        }
+        return ans;
+    }
+};
